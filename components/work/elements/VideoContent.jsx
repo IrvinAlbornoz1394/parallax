6c76a2ab8ad1cca37948aeaa73627f1ca81e7be3
IrvinAlbornoz <irvin.albornoz@hiumanlab.com>
2023-09-28 21:55:42 -0600

    merge video

--- components/work/elements/VideoContent.jsx
+++ components/work/elements/VideoContent.jsx
@@ -1,8 +1,6 @@
 import { Global, css } from '@emotion/react'
 import { Col, Typography } from 'antd'
 import React, { useState } from 'react'
-import { Col, Typography } from 'antd'
-import React, { useState } from 'react'
 import { PlayCircleOutlined } from '@ant-design/icons'
 import { useDispatch, useSelector } from 'react-redux'
 
