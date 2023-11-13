90525e7f992c4284add7ca9b53002db273ee1883
Irvin Michael Albornoz Vazquez <irvin.albornoz@hiumanlab.com>
2023-11-13 17:37:09 -0600

    Pantalla crew sakre

--- pages/crew/Item.jsx
+++ pages/crew/Item.jsx
@@ -1,8 +1,8 @@
 import MainLayout from '@/components/layaout/MainLayout'
-import { Col, Row, Typography } from 'antd'
+import { Col, Row, Space, Typography } from 'antd'
 import { useRouter } from 'next/router'
 import React, { useEffect } from 'react'
-import SakreAvatar from '../../assets/images/crew/sakre_avatar.png'
+import SakreAvatar from '../../assets/images/crew/sakre_avatar_.png'
 import { Global, css } from '@emotion/react'
 import { BehanceOutlined } from '@ant-design/icons';
 
@@ -45,25 +45,27 @@ const ItemCrew = () => {
     `} />
     <MainLayout >
       <div style={{position:'absolute', top:0, bottom: 0, left:0, right:0,  display: 'flex' }}>
-        <div style={{ marginTop:'auto', marginBottom:'auto', width:'100%', paddingTop:50, paddingBottom:50 }}>
+        <div style={{ marginTop:'auto', marginBottom:'auto', width:'100%' }}>
           <Row justify={'center'}>
             <Col xs={24} md={15} xl={20} xxl={15}>
               <Row justify={'start'}>
-                <Col >
-                  <img src={SakreAvatar.src} />  
-                </Col>
-                <Col span={15}>
-                  <Typography.Text style={{ fontSize:24, fontWeight:'bold', color:'white' }}>
-                    Hola me llamo
-                  </Typography.Text>
-                  <br />
-                  <Typography.Text style={{ fontSize:24, fontWeight:'bold', color:'white' }}>
-                    Sakre Saenz
-                  </Typography.Text>
-                  <br />
-                  <Typography.Text style={{ fontSize:16, color:'white' }}>
-                      Polvo de estrellas &#128125; / artbat  &#128165;  / street art &#128163;   / Interestelar &#127756;
-                  </Typography.Text>
+                <Col span={15} offset={1}>
+                  <Space size={30}>
+                    <img src={SakreAvatar.src} />  
+                    <div style={{ marginBottom:'auto', marginTop:'auto' }}>
+                      <Typography.Text style={{ fontSize:24, fontWeight:'bold', color:'white' }}>
+                        Hola me llamo
+                      </Typography.Text>
+                      <br />
+                      <Typography.Text style={{ fontSize:24, fontWeight:'bold', color:'white' }}>
+                        Sakre Saenz
+                      </Typography.Text>
+                      <br />
+                      <Typography.Text style={{ fontSize:16, color:'white' }}>
+                          Polvo de estrellas &#128125; / artbat  &#128165;  / street art &#128163;   / Interestelar &#127756;
+                      </Typography.Text>
+                      </div>
+                    </Space>
                 </Col>
                 <Col xs={24} md={13}>
                   <Typography.Text className='txt-white font-16' >
---
+++ pages/crew/sakre.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ItemCrew from './Item'
+
+const sakre = () => {
+  return (
+    <ItemCrew />
+  )
+}
+
+export default sakre
