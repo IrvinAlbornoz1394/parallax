f31cb3c75c27c43f1d86ebcdd41d51b0022a189e
Irvin Michael Albornoz Vazquez <irvin.albornoz@hiumanlab.com>
2024-01-04 02:50:15 -0600

    fix package

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,21 +1,7 @@
-/* import { EmailTemplate } from "../../components/EmailTemplate" */
-import { Resend } from "resend"
+import React from 'react'
 
-const resend = new Resend('re_VrfJd5pL_HTFDTYBMXATugMa3KLfEobbC');
-
-
-const Send =  async (req, res) => {
-    const { data, error } = await resend.emails.send({
-      from: 'onboarding@resend.dev',
-      to: 'irvinalbornoz1394@gmail.com',
-      subject: 'Hello World',
-      html: '<p>Congrats on sending your <strong>first email</strong>!</p>'
-    });
-
-  if (error) {
-    return res.status(400).json(error)
-  }
-  res.status(200).json(data)
+export const hello = () => {
+  return (
+    <div>hello</div>
+  )
 }
-
-export default Send
